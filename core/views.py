661ef6030604757cf4298bf9a7e7478f9dfc7615
Ximena <ximenandrea.ro@gmail.com>
2020-12-09 19:11:18 -0500

    serializers

--- core/views.py
+++ core/views.py
@@ -5,6 +5,9 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import PostSerializer
+from .models import Post
+
 
 #this view returns the data as a Json payload
 # def test_view(request):
@@ -16,9 +19,18 @@ from rest_framework.views import APIView
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            'name': 'jonh',
-            'age' : 23
-        
-        }
-        return Response(data)
+        qs = Post.objects.all()
+        post = qs.first()
+ #Return data for multiple instances       
+ # serializer = PostSerializer(qs, many=True)
+
+  #Return data for one instance
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.daa)
+        return Response(serializer.errors)
